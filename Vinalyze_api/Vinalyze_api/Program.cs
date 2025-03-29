b86f0f6acfc2dff522f68945f609d74a379a322a
ethanJlab <ethanjanicki89@gmail.com>
2025-03-29 17:37:42 -0400

    Create Wine entity, Crud, and migrations

--- Vinalyze_api/Vinalyze_api/Program.cs
+++ Vinalyze_api/Vinalyze_api/Program.cs
@@ -13,6 +13,10 @@ builder.Services.AddDbContext<AccountDbContext>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
+builder.Services.AddDbContext<WineDbContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ Vinalyze_api/Vinalyze_api/Wine.cs
@@ -0,0 +1,13 @@
+﻿namespace Vinalyze_api
+{
+    public class Wine
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; } // this will privide a short description about the history, creator, location, etc.
+
+        public string? FlavorProfile { get; set; } // this will be a description of the flavor profile of the wine
+    }
+}
