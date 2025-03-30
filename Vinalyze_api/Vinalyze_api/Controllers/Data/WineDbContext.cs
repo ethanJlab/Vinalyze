084c7f09cef16d487f76a7eda2b990ff85e2278e
ethanJlab <ethanjanicki89@gmail.com>
2025-03-30 15:10:49 -0400

    replace wine ids with a guid instead of an int

--- Vinalyze_api/Vinalyze_api/Controllers/Data/WineDbContext.cs
+++ Vinalyze_api/Vinalyze_api/Controllers/Data/WineDbContext.cs
@@ -14,7 +14,7 @@ namespace Vinalyze_api.Controllers.Data
                 new Wine
                 {
                     // id, Name, Description, FlavorProfile
-                    Id = 1,
+                    Id = new Guid("7c9e6679-7425-40de-944b-e07fc1f90ae7"),
                     Name = "Sample Wine",
                     Description = "This was created a plant somewhere on planet Earth. It is somewhere between 1 day old and 100 years old.",
                     FlavorProfile = "This tastes like dirt and feet."
