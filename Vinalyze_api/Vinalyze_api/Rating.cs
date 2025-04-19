e2dbb2980b2f1d441252df5913145e81ba7f39a5
ethanJlab <ethanjanicki89@gmail.com>
2025-04-19 14:59:34 -0400

    add ratings entity

---
+++ Vinalyze_api/Vinalyze_api/Rating.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vinalyze_api
+{
+    public class Rating
+    {
+        public Guid Id { get; set; }
+
+        // the account id of the user who made the rating
+        public Guid AccountId { get; set; }
+
+        // the wine id of the wine the rating is about
+        public Guid WineId { get; set; }
+
+        // the rating value, which must be between 1 and 5
+        [Range(1, 5, ErrorMessage = "Value must be between 1 and 5.")]
+        public int Value { get; set; }
+
+
+    }
+}
