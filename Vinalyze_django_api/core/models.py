4b32aa2735923d8bf78216d222da856a84e73b76
ethanJlab <ethanjanicki89@gmail.com>
2025-06-08 11:42:54 -0400

    add ondelete behaviors

--- Vinalyze_django_api/core/models.py
+++ Vinalyze_django_api/core/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.core.validators import validate_email, MaxValueValidator, MinValueValidator
-
 import uuid
 
 # Create your models here.
@@ -18,20 +17,20 @@ class Account(models.Model):
     UserName = models.CharField(max_length=200)
     Password = models.CharField(max_length=200)
     Email = models.CharField(validators = [validate_email])
-    LikedWines = models.ForeignKey(Wine)
+    LikedWines = models.ForeignKey(Wine,on_delete=models.SET_NULL)
     
     
 # Base ORM for the Wine Rating entity
 class Rating(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    AccountId = models.ForeignKey(Account)
-    WineId = models.ForeignKey(Wine)
+    AccountId = models.ForeignKey(Account,n_delete=models.CASCADE)
+    WineId = models.ForeignKey(Wine,n_delete=models.CASCADE)
     Value = models.IntegerField(MaxValueValidator(5), MinValueValidator(0))
 
-# Base ORm for the Comment entity
+# Base ORM for the Comment entity
 class Comment(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    AccountId = models.ForeignKey(Account)
-    WineId = models.ForeignKey(Wine)
+    AccountId = models.ForeignKey(Account, on_delete=models.CASCADE)
+    WineId = models.ForeignKey(Wine,on_delete=models.CASCADE)
     Text = models.CharField()
 
